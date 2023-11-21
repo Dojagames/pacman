d4b5d896f642fc38cfea5383a19504ff64287992
Jonas <dev.jonx.eu@gmail.com>
2023-11-21 21:07:46 +0100

    first commit

---
+++ client/src/App.vue
@@ -0,0 +1,55 @@
+<script>
+export default {
+    data() {
+        return {
+            
+        }
+    },
+    props: {
+        
+    },
+    components: {
+        
+    },
+    watch: {
+        
+    },
+    mount: {
+        
+    },
+    methods: {
+        
+    }
+}
+</script>
+
+<template>
+    <div class="pacman">
+      <div class="pacman-eye"></div>
+      <div class="pacman-mouth"></div>
+    </div>
+</template>
+
+<style scoped>
+    .pacman{
+      width: 100px;
+      height: 100px;
+      border-radius: 50%;
+      background-color: yellow;
+      position: absolute;
+    }
+
+    .pacman-eye{
+      
+    }
+
+    .pacman-eye{
+      position: relative;
+      width: 8px;
+      height: 8px;
+      border-radius: 50%;
+      background-color: #f1f1f1;
+      right: 20px;
+      top: 20px;
+    }
+</style>
